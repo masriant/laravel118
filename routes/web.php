5d2fde208066bfd403652c70df5a9cb5468ed0ec
Masrianto <45374438+masriant@users.noreply.github.com>
2024-05-30 19:03:21 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,23 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    return view('home');
-});
+// Route::get('/', function () {
+//     return view('home');
+// });
 
-Route::view('/', 'home');
+// Route::view('/', 'home');
+// Route::get('/', fn () => view('home'))->name('home');
+
+Route::get('/', fn () => view('home'));
+Route::get('/about', fn () => view('about'));
+Route::get('/contact', fn () => view('contact'));
+Route::get('/gallery', fn () => view('gallery'));
+
+Route::get('users', function () {
+    $users = [
+        ['id' => 1, 'name' => 'Masri'],
+        ['id' => 2, 'name' => 'Anto'],
+    ];
+
+    return $users;
+});
