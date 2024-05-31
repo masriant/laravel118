446e2ac1986e671cebd3f0676662e61892761617
Masrianto <45374438+masriant@users.noreply.github.com>
2024-05-31 09:00:03 +0700

    Facades

--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,4 @@ Route::get('/blog', [Controllers\BlogController::class, 'index']);
 Route::get('/about', [Controllers\AboutController::class, 'index']);
 Route::get('/contact', [Controllers\ContactController::class, 'index']);
 Route::get('/gallery', [Controllers\GalleryController::class, 'index']);
-
-Route::get('users', function () {
-    $users = [
-        ['id' => 1, 'name' => 'Masri'],
-        ['id' => 2, 'name' => 'Anto'],
-    ];
-
-    return view('users.index', compact('users'));
-});
+Route::get('/users', [Controllers\UserController::class, 'index']);
